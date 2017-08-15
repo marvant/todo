978de66d8e0ae8a350eae0194667581b27b56573
marvan_t <marvan_t@hotmail.com>
2017-08-14 21:20:24 -0700

    add delete task functinality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,6 +45,13 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+
+		redirect_to tasks_path
+	end
+
 	private
 	def task_params
 		params.require(:task).permit(:user_id, :title, :deadline, :completed, :detail)
