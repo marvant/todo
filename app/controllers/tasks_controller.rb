4e088f5513556f8e1be39f989321945ca26db9db
marvan_t <marvan_t@hotmail.com>
2017-08-14 19:30:03 -0700

    define new and create in task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
 class TasksController < ApplicationController
+	before_filter :authenticate_user!
 	def new
+		@task = Task.new
+	end
+
+	def create
+		@task = Task.new(task_params)
+		@user = current_user
+		@task.user_id = @user.id
+		if @task.save
+			redirect_to tasks_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def task_params
+		params.require(:task).permit(:user_id, :title, :deadline, :completed, :detail)
 	end
 end
