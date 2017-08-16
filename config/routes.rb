3f95c81dd684314a36f3858338ff072d85073554
marvan_t <marvan_t@hotmail.com>
2017-08-15 18:06:53 -0700

    redirect root to signin or task index if signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+
+  authenticated :user do
+  	root :to => redirect("/tasks")
+  end
+
+  root :to => redirect("/users/sign_in")
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  # root 'welcome#index'
 
   resources :tasks
 end
